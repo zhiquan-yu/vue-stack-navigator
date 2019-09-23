92da9e31f2f9abe249c352d44773e26314ce3034
fuzhu <fuzhu@xiaohongshu.com>
2019-09-23 14:48:43 +0800

    feat: add overflow for screen

--- src/AppContainer.vue
+++ src/AppContainer.vue
@@ -1,18 +1,87 @@
 <template>
   <transition :name="transitionName" @afterEnter="handleAfterEnter">
     <StackKeepAlive>
-      <RouterView ref="screen" :key="$route.path" class="router-view" />
+      <RouterView ref="screen" :key="route.path" class="screen" />
     </StackKeepAlive>
   </transition>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import { createComponent, ref, watch, SetupContext } from "@vue/composition-api";
+import { createComponent, ref, watch, SetupContext, Ref, computed } from "@vue/composition-api"
 import StackKeepAlive from './components/StackKeepAlive'
+import { Route } from 'vue-router'
 
 enum NavigationLifecycle {
   AfterEnter = 'afterEnter',
+}
+
+function callHook(component: Vue, hookName: NavigationLifecycle) {
+  const hook = component.$options[hookName]
+
+  if (hook) {
+    hook.call(component)
+  }
+
+  component.$children.forEach(child => callHook(child, hookName))
+}
+
+function useTransition(route: Ref<Route>) {
+  const transitionName = ref('')
+    
+  let touchstart: TouchEvent | null = null
+  let touchmove: TouchEvent | null = null
+
+  let fromTime = 0
+  let toTime = 0
+
+  document.body.addEventListener('touchstart', e => {
+    touchstart = e
+  })
+
+  document.body.addEventListener('touchmove', e => {
+    touchmove = e
+  })
+
+  watch(route, () => {
+    fromTime = toTime
+    toTime = window.history.state ? (Number.parseInt(window.history.state.key, 0) || 0) : 0
+
+    if (!toTime && !fromTime) {
+      return
+    }
+
+    if (toTime < fromTime) {
+      if (touchstart && touchmove && touchstart.touches[0].clientX < 50 && touchmove.touches[0].clientX < 0) {
+        transitionName.value = 'slide-right-gesture'
+      } else {
+        transitionName.value = 'slide-right'
+      }
+    } else {
+      if (touchstart && touchmove && document.body.clientWidth - touchstart.touches[0].clientX < 50 && touchmove.touches[0].clientX - touchstart.touches[0].clientX < -10) {
+        transitionName.value = 'slide-left-gesture'
+      } else {
+        transitionName.value = 'slide-left'
+      }
+    }
+  }, {
+    flush: 'pre',
+  })
+
+  return {
+    transitionName,
+  }
+}
+
+function useLifecycles(screen: Ref<Vue>) {
+  return {
+    afterEnter() {
+      // FIXME: afterEnter called before to screen entered
+      setTimeout(() => {
+        callHook(screen.value, NavigationLifecycle.AfterEnter)
+      }, 100)
+    },
+  }
 }
 
 export default createComponent({
@@ -21,68 +90,17 @@ export default createComponent({
   setup(_: any, context: SetupContext) {
     const { root } = context
 
+    const route = computed(() => root.$route)
     const screen = ref<Vue>(null)
-    const transitionName = ref('')
-    
-    let touchstart: TouchEvent | null = null
-    let touchmove: TouchEvent | null = null
-
-    let fromTime = 0
-    let toTime = 0
-
-    document.body.addEventListener('touchstart', e => {
-      touchstart = e
-    })
-
-    document.body.addEventListener('touchmove', e => {
-      touchmove = e
-    })
-
-    watch(() => root.$route, () => {
-      fromTime = toTime
-      toTime = window.history.state ? (Number.parseInt(window.history.state.key, 0) || 0) : 0
-
-      if (!toTime && !fromTime) {
-        return
-      }
-
-      if (toTime < fromTime) {
-        if (touchstart && touchmove && touchstart.touches[0].clientX < 50 && touchmove.touches[0].clientX < 0) {
-          transitionName.value = 'slide-right-gesture'
-        } else {
-          transitionName.value = 'slide-right'
-        }
-      } else {
-        if (touchstart && touchmove && document.body.clientWidth - touchstart.touches[0].clientX < 50 && touchmove.touches[0].clientX - touchstart.touches[0].clientX < -10) {
-          transitionName.value = 'slide-left-gesture'
-        } else {
-          transitionName.value = 'slide-left'
-        }
-      }
-    }, {
-      flush: 'pre',
-    })
-
-    function callHook(component: Vue, hookName: NavigationLifecycle) {
-      const hook = component.$options[hookName]
-
-      if (hook) {
-        hook.call(component)
-      }
-
-      component.$children.forEach(child => callHook(child, hookName))
-    }
+    const { transitionName } = useTransition(route)
+    const { afterEnter } = useLifecycles(screen)
 
     return {
       screen,
+      route,
       transitionName,
 
-      handleAfterEnter() {
-        // FIXME: afterEnter called before to screen entered
-        setTimeout(() => {
-          callHook(screen.value, NavigationLifecycle.AfterEnter)
-        }, 100)
-      },
+      handleAfterEnter: afterEnter,
     }
   },
 
@@ -93,11 +111,15 @@ export default createComponent({
 </script>
 
 <style scoped>
-.router-view {
+.screen {
   --duration: .3s;
   
   position: absolute;
   top: 0;
+  width: 100vw;
+  height: 100vh;
+  overflow-y: auto;
+  -webkit-overflow-scrolling: touch;
 
   will-change: transform;
   -webkit-backface-visibility: hidden;
